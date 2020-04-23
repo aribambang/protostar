d0904adb254900c7e156bb1fe0eaa40f3e3441ba
Ari Bambang Kurniawan <aribk210@gmail.com>
2020-04-23 22:22:01 +0700

    custom document page

---
+++ pages/_document.js
@@ -0,0 +1,26 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang='id'>
+        <Head>
+          <meta charSet='UTF-8' />
+          <meta name='description' content='Protostar' />
+          <link
+            rel='stylesheet'
+            href='https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css'
+            integrity='sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh'
+            crossOrigin='anonymous'
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
